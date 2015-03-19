7ee733a2b7d3b8539ff122cc373982fa7520d1d6
phongleland <phongleland@Phongs-iMac.local>
2015-03-19 00:16:38 -0700

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,16 +6,18 @@ set :stages, %w(production)
 set :default_stage, 'production'
 
 set :application, "oclef"
-set :deploy_to, "/var/www/#{application}"
 set :scm, :git
-set :use_sudo, false
-set :domain, "ec2-52-1-141-202.compute-1.amazonaws.com"
-set :rails_env, 'development'
+set :repository,  "git@github.com:phongleland/oclef.git"
 set :branch, 'master'
+set :deploy_via, :remote_cache
 
 default_run_options[:pty] = true
 set :ssh_options, {:forward_agent => true, :keys => %w(/Users/phongleland/.ssh/oclef)}
-
 set :user, 'deployer'
+set :use_sudo, false
+set :domain, "ec2-52-1-141-202.compute-1.amazonaws.com"
+set :rails_env, 'development'
 
+
+set :deploy_to, "/var/www/#{application}"
 server 'ec2-52-1-141-202.compute-1.amazonaws.com', :app, :web, :db, :primary => true
